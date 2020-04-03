419c7beb105ecf6267178b2e7cdb1d24f67c1ef4
Sankalp Sinha <sankalpsinha.cmos@gmail.com>
2020-04-03 23:37:10 +0200

    Daily Prac

---
+++ bt/z/autogit.sh
@@ -0,0 +1,8 @@
+echo "Auto Git-Hub Push Script Initiated !!"
+cd ~/OneDrive/code/leetcode_grind/
+git status
+git add .
+echo "All changes added to commit queue..."
+date = %DATE%
+git commit -m "Daily Prac $date"
+
