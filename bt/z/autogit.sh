173eac778ca1741bb0b5161108a1c2074720dc27
Sankalp Sinha <sankalpsinha.cmos@gmail.com>
2020-04-03 23:44:15 +0200

    echo

--- bt/z/autogit.sh
+++ bt/z/autogit.sh
@@ -3,8 +3,10 @@ cd ~/OneDrive/code/leetcode_grind/
 git status
 git add .
 echo "All changes added to commit queue..."
-date = %DATE%
-git commit -m "Daily Prac $date"
+set date = %date%
+message = "Daily Prac $date"
+echo $message
+git commit -m echo $message
 echo "Git commit successful..."
 git push
 echo "Git push successfull..."
