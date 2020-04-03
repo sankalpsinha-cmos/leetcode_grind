11a1999abec44e53f8bbc8313391803ca8d0949d
Sankalp Sinha <sankalpsinha.cmos@gmail.com>
2020-04-03 23:54:39 +0200

    Daily Prac

--- bt/z/autogit.sh
+++ bt/z/autogit.sh
@@ -1,9 +1,11 @@
 printf "\n\nAuto Git-Hub Push Script Initiated !!\n\n"
 cd ~/OneDrive/code/leetcode_grind/
 git status
-git add -a
+read
 printf "\n\nAll changes added to commit queue...\n\n"
 git commit -a -m "Daily Prac"
+read
 printf "\n\n Git Commit successful...\n\n"
 git push
+read
 printf "\n\nGit push successfull...\n\n"
